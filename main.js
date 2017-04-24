b8c611390d233e9755a4a074d90a2356a5078753
NICHOLAS INVERNO <NICHOLAS@Nicks-MacBook-Air.local>
2017-04-24 11:13:35 -0700

    Added planets array

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 // Write your JavaScript code here
+console.log("Hey");
+
+function calculateWeight(){
+var weight = document.getElementsByClassName('myWeight').value;
+
+}
+
+var planets = [
+  {planet: 'Sun', gravity: 27.9},
+  {planet: 'Mercury', gravity: 0.377},
+  {planet: 'Venus', gravity: 0.9032},
+  {planet: 'Earth', gravity: 1},
+  {planet: 'Moon', gravity: 0.1655},
+  {planet: 'Mars', gravity: 0.3895},
+  {planet: 'Jupiter', gravity: 2.640},
+  {planet: 'Saturn', gravity: 1.139},
+  {planet: 'Uranus', gravity: 0.917},
+  {planet: 'Neptune', gravity: 1.148},
+  {planet: 'Pluto', gravity: 0.06}
+];
+for (x = 0; x <= planets.length; x++){
+  console.log(planets[x])
+}
