b3821cb8121b3e5d1ab410d7eb21a30d9e255187
NICHOLAS INVERNO <NICHOLAS@Nicks-MacBook-Air.local>
2017-04-24 14:03:06 -0700

    Added code to dynamically generate planets dropdown

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 // Write your JavaScript code here
-console.log("Hey");
+var planets = [
+  {'Sun', 27.9},
+  {'Mercury', 0.377},
+  {'Venus', 0.9032},
+  {'Earth', 1},
+  {'Moon', 0.1655},
+  {'Mars', 0.3895},
+  {'Jupiter', 2.640},
+  {'Saturn', 1.139},
+  {'Uranus', 0.917},
+  {'Neptune', 1.148},
+  {'Pluto', 0.06}
+];
 
-function calculateWeight(){
-var weight = document.getElementsByClassName('myWeight').value;
-
+var select = document.getElementById('planetSelect');
+for (i=0; i<= planets.length;i++){
+  var planetName = planet[i][0];
+  var opt = document.createElement('option')
+  opt.textContext = planetName;
+  opt.value = planetName;
+  select.appendChild(opt);
 }
 
-var planets = [
-  {planet: 'Sun', gravity: 27.9},
-  {planet: 'Mercury', gravity: 0.377},
-  {planet: 'Venus', gravity: 0.9032},
-  {planet: 'Earth', gravity: 1},
-  {planet: 'Moon', gravity: 0.1655},
-  {planet: 'Mars', gravity: 0.3895},
-  {planet: 'Jupiter', gravity: 2.640},
-  {planet: 'Saturn', gravity: 1.139},
-  {planet: 'Uranus', gravity: 0.917},
-  {planet: 'Neptune', gravity: 1.148},
-  {planet: 'Pluto', gravity: 0.06}
-];
-for (x = 0; x <= planets.length; x++){
-  console.log(planets[x])
+
+
+
+
+
+document.getElementById("output").innerHTML = story;
+console.log(planets[i])
+
+
+document.createElement()
+}
+
+
+
+
+f
+int[][] myArray = new int[cols][rows];
+// Two nested loops allow us to visit every spot in a 2D array.
+// For every column I, visit every row J.
+for (int i = 0; i < cols; i++) {
+  for (int j = 0; j < rows; j++) {
+    myArray[i][j] = 0;
+  }
 }
