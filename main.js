f5a2fc7397bc9b32c9e04ecc1080fbd5735ca629
NICHOLAS INVERNO <NICHOLAS@Nicks-MacBook-Air.local>
2017-04-24 18:10:30 -0700

    Added core business logic

--- main.js
+++ main.js
@@ -1,48 +1,37 @@
 // Write your JavaScript code here
 var planets = [
-  {'Sun', 27.9},
-  {'Mercury', 0.377},
-  {'Venus', 0.9032},
-  {'Earth', 1},
-  {'Moon', 0.1655},
-  {'Mars', 0.3895},
-  {'Jupiter', 2.640},
-  {'Saturn', 1.139},
-  {'Uranus', 0.917},
-  {'Neptune', 1.148},
-  {'Pluto', 0.06}
+  ['Pluto', 0.06],
+  ['Neptune', 1.148],
+  ['Uranus', 0.917],
+  ['Saturn', 1.139],
+  ['Jupiter', 2.640],
+  ['Mars', 0.3895],
+  ['Moon', 0.1655],
+  ['Earth', 1],
+  ['Venus', 0.9032],
+  ['Mercury', 0.377],
+  ['Sun', 27.9]
 ];
 
 var select = document.getElementById('planetSelect');
-for (i=0; i<= planets.length;i++){
-  var planetName = planet[i][0];
+for (i=0; i<planets.length;i++){
+  var planetName = planets[i][0];
   var opt = document.createElement('option')
-  opt.textContext = planetName;
-  opt.value = planetName;
+  opt.innerText = planets[i][0];
+  opt.value = planets[i][1];
+  opt.textContent = planetName;
   select.appendChild(opt);
 }
 
-
-
-
-
-
-document.getElementById("output").innerHTML = story;
-console.log(planets[i])
-
-
-document.createElement()
+document.getElementById('calculateWeight').onclick = function handleClickEvent(ev){
+  alert('Hello!');
+  var userWeight = document.getElementById('userWeight').value;
+  var userChoice = document.getElementById('planetSelect').selectedIndex;
+  var gravity = document.getElementById('planetSelect').value;
+  console.log('Your ' + 'weight ' + 'on ' + 'planet ' + userChoice + ' is ' + (userWeight * gravity) + '!');
 }
 
 
-
-
-f
-int[][] myArray = new int[cols][rows];
-// Two nested loops allow us to visit every spot in a 2D array.
-// For every column I, visit every row J.
-for (int i = 0; i < cols; i++) {
-  for (int j = 0; j < rows; j++) {
-    myArray[i][j] = 0;
-  }
-}
+//var planetName = document.getElementById('planetSelect').options[userChoice];
+//var sel(Index) = document.getElementById('plenetSelect').selectIndex;
+//var selGravity = document.getElementById('planetSelect')[select index].value
