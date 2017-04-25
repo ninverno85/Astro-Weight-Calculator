6601ab75a8321257c1312914a202e442c306d569
NICHOLAS INVERNO <NICHOLAS@Nicks-MacBook-Air.local>
2017-04-25 11:22:24 -0700

    Added code to dyanamically generate planets dropdown

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Write your JavaScript code here
+// Adding the planets array
 var planets = [
   ['Pluto', 0.06],
   ['Neptune', 1.148],
@@ -12,3 +12,14 @@ var planets = [
   ['Mercury', 0.377],
   ['Sun', 27.9]
 ];
+
+//Adding code to dynamically generate planets dropdown
+var select = document.getElementById('planets');
+for (i=0; i<planets.length;i++){
+  var planetName = planets[i][0];
+  var opt = document.createElement('option')
+  opt.innerText = planets[i][0];
+  opt.value = planets[i][1];
+  opt.textContent = planetName;
+  select.appendChild(opt);
+}
